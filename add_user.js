0f3999915373a7b6afd14668f20527ffee50c03e
Ericksulino <ericksulino@gmail.com>
2023-03-20 20:12:53 -0300

    add

--- add_user.js
+++ add_user.js
@@ -1,37 +1,25 @@
-const { Wallets, X509WalletMixin } = require('fabric-network');
+const { Wallets} = require('fabric-network');
 const path = require('path');
 const fs = require('fs');
 
+
 async function main() {
-  try {
-    // Define o diretório da carteira e o nome do usuário
-    const walletPath = path.join(__dirname, 'wallet');
-    const userId = 'user1';
+    const wallet = await Wallets.newFileSystemWallet('wallet');
 
-    // Carrega a carteira
-    const wallet = await Wallets.newFileSystemWallet(walletPath);
+    const cert = fs.readFileSync('../caliper-workspace/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/User1@org1.example.com-cert.pem').toString();
+    const key = fs.readFileSync('../caliper-workspace/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/priv_sk').toString();
 
-    // Verifica se a carteira já possui a identidade do usuário
-    const identity = await wallet.get(userId);
-    if (identity) {
-      console.log(`A identidade ${userId} já existe na carteira`);
-      return;
-    }
+    const identityLabel = 'User1@org1.example.com';
+    const identity = {
+        credentials: {
+            certificate: cert,
+            privateKey: key,
+        },
+        mspId: 'Org1MSP',
+        type: 'X.509',
+    };
 
-    // Lê o arquivo de chave privada e o arquivo de certificado público
-    const privateKey = fs.readFileSync('../caliper-workspace/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/priv_sk').toString();
-    const certificate = fs.readFileSync('../caliper-workspace/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/User1@org1.example.com-cert.pem').toString();
-
-    // Cria uma nova identidade de carteira
-    const identityLabel = `${userId}@org1.example.com`;
-    const mixin = X509WalletMixin.createIdentity('Org1MSP', certificate, privateKey);
-    await wallet.put(identityLabel, mixin);
-
-    console.log(`A identidade ${identityLabel} foi adicionada à carteira`);
-  } catch (error) {
-    console.error(`Falha ao adicionar a identidade à carteira: ${error}`);
-    process.exit(1);
-  }
+    await wallet.put(identityLabel, identity);
 }
 
 main();
