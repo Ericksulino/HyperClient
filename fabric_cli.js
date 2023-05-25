0fb1372871798d56263b28197c97a78ee5d91b81
ericksulino <ericksulino@gmail.com>
2023-05-25 19:04:25 -0300

    att

--- fabric_cli.js
+++ fabric_cli.js
@@ -23,7 +23,7 @@ const submitTransaction = async (contract) => {
     const transaction = contract.createTransaction('createCar');
 
     // Define os endossadores da transação
-    transaction.setEndorsingPeers(['peer0.org1.example.com']);
+    transaction.setEndorsingPeers(['peer0.org1.example.com','peer1.org1.example.com']);
 
     // Endossa a proposta de transação
     const endorsement = await transaction.submit('CAR34', 'VW', 'Polo', 'Grey', 'Mary');
