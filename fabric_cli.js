68b354d8a7511f42881156472ed9183bf9b0b895
ericksulino <ericksulino@gmail.com>
2023-06-01 19:01:59 -0300

    att

--- fabric_cli.js
+++ fabric_cli.js
@@ -21,13 +21,7 @@ const generateRandomHash = () => {
 const submitTransactionEndorse = async (contract) => {
   try {
     const transaction = contract.createTransaction('createCar');
-    transaction.setTransient({
-      CAR_ID: 'CAR404',
-      MAKE: 'Toyota',
-      MODEL: 'Supra',
-      COLOR: 'Orange',
-      OWNER: 'Brian'
-    });
+    transaction.setArgs('CAR404', 'Toyota', 'Supra', 'Orange', 'Brian');
 
     const proposalResponse = await transaction.evaluate();
     if (proposalResponse) {
