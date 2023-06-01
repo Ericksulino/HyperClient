74a7777843b25e689af1fdb3ee092da1b9bd4611
ericksulino <ericksulino@gmail.com>
2023-06-01 19:16:09 -0300

    add endorsement and endorser

--- fabric_cli.js
+++ fabric_cli.js
@@ -1,4 +1,4 @@
-const { Gateway, Wallets } = require('fabric-network');
+const { Gateway, Wallets ,Endorsement, Endorser} = require('fabric-network');
 const crypto = require('crypto');
 const path = require('path');
 const fs = require('fs');
