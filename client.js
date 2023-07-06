ba1c95c5a3dd5d43ef6e6abcd445557441401ae1
Ericksulino <ericksulino@gmail.com>
2023-07-06 15:04:06 -0300

    update

--- client.js
+++ client.js
@@ -160,7 +160,7 @@ const main = async () =>{
     await gateway.connect(ccp, {
       wallet,
       identity: userId,
-      discovery: { enabled: true, asLocalhost: false },
+      discovery: { enabled: true, asLocalhost: true},
     });
 
 
